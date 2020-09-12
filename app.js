e00dfdfe8c45ac06f42f331dd7bea852422ff026
Rahul Garg <rahulgarg0697@gmail.com>
2020-09-12 07:09:54 +0000

    add start script for heroku and modify app.listen port

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use("/campgrounds" , campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-app.listen(3000, function(){
-	console.log("The YelpCamp Server Has Started!")
-})
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("The YelpCamp Server Has Started!");
+});
